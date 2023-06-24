6f4172dec29b5dc77f3397256eede9d3dcad207e
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-06-24 20:17:23 +0430

    Bubble & Quick Sort

---
+++ Arrays And Lists/Soritng_Algorithms/Bubble Sort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Arrays_And_Lists.Soritng_Algorithms
+{
+    public static class Bubble_Sort
+    {
+        private static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+        public static class BubbleSort
+        {
+            public static void Sort<T>(T[] array) where T : IComparable
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (array[j].CompareTo(array[j + 1]) > 0)
+                        {
+                            Swap(array, j, j + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Arrays And Lists/Soritng_Algorithms/Quick Sort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Arrays_And_Lists.Soritng_Algorithms
+{
+    public static class Quick_Sort
+    {
+        private static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+        private static int Partition<T>(T[] array, int lower, int upper)
+            where T : IComparable
+        {
+            int i = lower;
+            int j = upper;
+            T pivot = array[lower];
+            do
+            {
+                while (array[i].CompareTo(pivot) < 0) { i++; }
+                while (array[j].CompareTo(pivot) > 0) { j--; }
+                if (i >= j) { break; }
+                Swap(array, i, j);
+            }
+            while (i <= j);
+            return j;
+        }
+
+        private static T[] Sort<T>(T[] array, int lower, int upper)
+            where T : IComparable
+        {
+            if (lower < upper)
+            {
+                int p = Partition(array, lower, upper);
+                Sort(array, lower, p);
+                Sort(array, p + 1, upper);
+            }
+            return array;
+        }
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+    }
+}
