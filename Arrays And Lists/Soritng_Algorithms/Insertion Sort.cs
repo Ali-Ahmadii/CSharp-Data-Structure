97632f886409d698fb0868a520ab0717c435c701
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-06-24 13:59:48 +0430

    update

---
+++ Arrays And Lists/Soritng_Algorithms/Insertion Sort.cs
@@ -0,0 +1,27 @@
+﻿namespace DS.Arrays_And_Lists.Soritng_Algorithms
+{
+    public static class Insertion_Sort
+    {
+        private static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j].CompareTo(array[j - 1]) < 0)
+                {
+                    Swap(array, j, j - 1);
+                    j--;
+                }
+            }
+            {
+
+            }
+        }
+    }
+}
