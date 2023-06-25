13436ccab50d1256a1104cbc93fe406d2f7ebc3d
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-06-25 20:58:54 +0430

    Stacks and Queues

---
+++ Stacks and Queues/Queues/Queues.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Stacks_and_Queues.Queues
+{
+    public class Queues
+    {
+       /* static void Main(string[] args)
+        {
+            //FIFO
+            Queue<int> callerIds = new Queue<int>();
+            callerIds.Enqueue(1);
+            callerIds.Enqueue(2);
+            callerIds.Enqueue(3);
+            callerIds.Enqueue(4);
+            foreach (var id in callerIds)
+            {
+                Console.WriteLine(id);
+            }
+            // Dequeue Returns an item from the beginning of the queue and removes it from the queue.
+            //Peek()  Returns an first item from the queue without removing it.
+         // Contains(T) Checks whether an item is in the queue or not
+        // Clear() Removes all the items from the queue.
+        }
+       */
+    }
+}
---
+++ Stacks and Queues/Stacks/stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Stacks_and_Queues.Stacks
+{
+    public class stack
+    {
+       /* static void Main(string[] args)
+        {
+            //LIFO
+            Stack<string> names = new Stack<string>();
+            names.Push("Ali");
+            names.Push("X");
+            names.Push("Y");
+            string name = names.Pop();
+            Console.WriteLine(name);
+        }
+       */
+    }
+}
